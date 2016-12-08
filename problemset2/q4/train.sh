95c77ca7796b70b2bcf4e7efed4f95788250b348
Dylan Price <the.dylan.price@gmail.com>
2016-12-07 16:26:01 -0800

    Add -v and -o options to weka classification to simplify output.

--- problemset2/q4/train.sh
+++ problemset2/q4/train.sh
@@ -3,4 +3,4 @@
 #classifier="weka.classifiers.bayes.NaiveBayesMultinomial"
 classifier="weka.classifiers.functions.SMO"
 
-java -cp ~/Downloads/weka-3-8-0/weka.jar $classifier -t $1 -T spam_test.arff # > "$1_results.txt"
+java -cp ~/Downloads/weka-3-8-0/weka.jar $classifier -t $1 -T spam_test.arff -v -o # > "$1_results.txt"
