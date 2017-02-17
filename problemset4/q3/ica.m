9699b317216ce01d8c066487318eecffd0682ef7
Dylan Price <the.dylan.price@gmail.com>
2017-02-16 22:08:42 -0800

    Implemented ica, still need to work out perf.

--- problemset4/q3/ica.m
+++ problemset4/q3/ica.m
@@ -1,29 +1,46 @@
 function W = ica(X)
 
-    [n, m] = size(X)
-    W = zeros(n, n)
-    %do
+    count = 1
+    begin = time
+
+    X = X'
+    [m, n] = size(X)
+    W = eye(n, n)
+    chunk_size = 100
+    num_chunks = ceil(m / chunk_size)
+    do
         old_W = W(:, :)
-        order = randperm(m)
-        X_shuffled = X(:, order)
-        
-    %until (converged(W, old_W))
+        X_shuffled = X(randperm(m), :)
+        for i = 1:num_chunks
+            stop = i * chunk_size
+            start = stop - (chunk_size - 1)
+            X_chunk = X_shuffled(start:min(stop, m), :)
+            W = update_W(W, X_chunk)
+            if (is_converged(W, old_W))
+                break
+            endif
+        endfor
+        count += 1
+    until (is_converged(W, old_W))
+
+    printf("took: %d  count: %d\n", time - begin, count)
+
 endfunction
 
 
-function new_W = update_W(W, X_chunk)
+function new_W = update_W(W, X)
 
-    % placeholder
-    new_W = W
+    alpha = 0.0005
+    new_W = W + alpha * ((1 - 2 * sigmoid(W * X')) * X + inv(W'))
 
 endfunction
 
 
-function a = converged(W, old_W)
+function converged = is_converged(W, old_W)
 
     delta = abs(W - old_W)
-
-    a = delta < .00001
+    %converged = delta < .00001
+    converged = delta < .001
 
 endfunction
 
