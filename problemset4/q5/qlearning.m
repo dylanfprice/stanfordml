d72d2e0929ac684a3fedde4b53f3ee34f16f1622
Dylan Price <the.dylan.price@gmail.com>
2017-02-26 17:00:16 -0800

    Implemented q learning algorithm for problem 5.

--- problemset4/q5/qlearning.m
+++ problemset4/q5/qlearning.m
@@ -1,16 +1,42 @@
 function [q, steps_per_episode] = qlearning(episodes)
 
 % set up parameters and initialize q values
-alpha = 0.05;
-gamma = 0.99;
-num_states = 100;
-num_actions = 2;
-actions = [-1, 1];
-q = zeros(num_states, num_actions);
+alpha = 0.05
+gamma = 0.99
+num_states = 100
+num_actions = 2
+actions = [-1, 1]
+q = zeros(num_states, num_actions)
 
-for i=1:episodes,
-  [x, s, absorb] =  mountain_car([0.0 -pi/6], 0);
-  %%% YOUR CODE HERE
+steps_per_episode = zeros(episodes, 1)
+state_top = num_states
+rewards = -1 * ones(num_states, 1)
+rewards(state_top) = 0
+
+for i=1:episodes
+  [x, state, absorb] =  mountain_car([0.0 -pi/6], 0)
+
+  while (state != state_top)
+    action = choose_action(q, state)
+    [new_x, new_state, new_absorb] = mountain_car(x, actions(action))
+    steps_per_episode(i)++
+    q(state, action) = (1 - alpha) * q(state, action) + gamma * (rewards(new_state) + gamma * max(q(new_state, :)))
   
-  
+    x = new_x
+    state = new_state 
+    absorb = new_absorb
+  endwhile
+
 end
+
+endfunction
+
+
+function action = choose_action(q, state)
+  q_values = q(state, :)
+  if (q_values == q_values(1))
+    action = randi(length(q_values))
+  else
+    [max_q, action] = max(q_values)
+  endif
+endfunction
