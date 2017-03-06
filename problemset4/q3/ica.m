be0d256e1dd9af8a424bfc2c55e9eb04f4d7972f
Dylan Price <the.dylan.price@gmail.com>
2017-03-05 17:30:43 -0800

    Instead of checking for convergence just do a fixed number of passes.

--- problemset4/q3/ica.m
+++ problemset4/q3/ica.m
@@ -5,19 +5,17 @@ function W = ica(X)
     W = eye(n, n)
     chunk_size = 100
     num_chunks = ceil(m / chunk_size)
-    do
+    for j = 1:10
         old_W = W(:, :)
         X_shuffled = X(randperm(m), :)
         for i = 1:num_chunks
             stop = i * chunk_size
             start = stop - (chunk_size - 1)
             delta = compute_delta(W, X_shuffled(start:min(stop, m), :))
-            if (is_converged(delta))
-                break
             endif
             W += delta
         endfor
-    until (is_converged(delta))
+    endfor
 
 endfunction
 
@@ -26,14 +24,6 @@ function delta = compute_delta(W, X)
 
     alpha = 0.0005
     delta = alpha * ((1 - 2 * sigmoid(W * X')) * X + inv(W'))
-
-endfunction
-
-
-function converged = is_converged(delta)
-
-    % try to get to .00001
-    converged = all(all(abs(delta) < .001))
 
 endfunction
 
