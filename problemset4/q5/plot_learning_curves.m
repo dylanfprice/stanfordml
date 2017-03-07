a6292b911f77bf3339231e93b7101636460cdc5b
Dylan Price <the.dylan.price@gmail.com>
2017-03-06 19:24:56 -0800

    Revert plot_learning_curves to original version.

--- problemset4/q5/plot_learning_curves.m
+++ problemset4/q5/plot_learning_curves.m
@@ -1,7 +1,5 @@
-all_ep_steps = zeros(20, 100);
-parfor i = 1:20
-  [q, ep_steps] = qlearning(100);
-  all_ep_steps(i, :) = ep_steps;
-endparfor
-
-plot(mean(all_ep_steps))
+for i = 1:10
+  [q, ep_steps] = qlearning(10000)
+  all_ep_steps(i, :) = ep_steps
+endfor
+plot(mean(reshape(mean(all_ep_steps), 500, 20)))
