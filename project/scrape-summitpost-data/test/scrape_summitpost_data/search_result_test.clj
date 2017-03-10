78f0c487ed8d91a95ab7d2a9afbf8396fa02b306
Dylan Price <the.dylan.price@gmail.com>
2017-03-09 21:22:24 -0800

    Finish unit tests for search_result.

--- project/scrape-summitpost-data/test/scrape_summitpost_data/search_result_test.clj
+++ project/scrape-summitpost-data/test/scrape_summitpost_data/search_result_test.clj
@@ -2,6 +2,45 @@
   (require [clojure.test :refer [deftest is]]
            [scrape-summitpost-data.search-result :as test-ns]))
 
+(deftest ensure-sequence-test
+  (is (= [1 2 3] (#'test-ns/ensure-sequence [1 2 3]))
+      "returns sequence when given a sequence")
+  (is (= [1] (#'test-ns/ensure-sequence 1))
+      "returns sequence when given an int"))
+
+(deftest extract-result-links-test
+  (let [jsoup-snippet (reaver/parse 
+                        "<table class='srch_results'>
+                           <tbody>
+                           <tr>
+                             <td class='srch_results_lft'></td>
+                             <td class='srch_results_rht'>
+                               <a href='/test-item-name/12345'></a>
+                             </td>
+                           </tr>
+                           </tbody>
+                         </table>")]
+    (is (= "/test-item-name/12345"
+           (#'test-ns/extract-result-links jsoup-snippet))
+        "extracts link to item from search results table")))
+
+(deftest extract-pager-links-test
+  (let [jsoup-snippet (reaver/parse
+                        "<td>
+                         <a href='/test?foo=bar&page=1' class='pagertext'></a>
+                         </td>")]
+    (is (= ["/test?foo=bar&page=1"]
+           (#'test-ns/extract-pager-links jsoup-snippet))
+        "extracts pager link"))
+  (let [jsoup-snippet (reaver/parse
+                        "<td>
+                         <a href='/test?foo=bar&page=1' class='pagertext'></a>
+                         <a href='/test?foo=bar' class='pagertext'></a>
+                         </td>")]
+    (is (= ["/test?foo=bar&page=1"]
+           (#'test-ns/extract-pager-links jsoup-snippet))
+        "ignores links without page param")))
+
 (deftest extract-last-page-test
   (is (= 1
          (#'test-ns/extract-last-page ["http://example.org?page=1"]))
@@ -14,3 +53,20 @@
   (is (thrown? java.lang.NumberFormatException
          (#'test-ns/extract-last-page ["http://example.org?page=f"]))
       "fails on a non-integer value of page param"))
+
+(deftest extract-all-pager-links-test
+  (let [jsoup-snippet (reaver/parse
+                        "<td>
+                         <a href='/test?page=1' class='pagertext'></a>
+                         <a href='/test?page=2' class='pagertext'></a>
+                         <a href='/test?page=6' class='pagertext'></a>
+                         </td>")]
+    (is (= ["/test?page=1", "/test?page=2", "/test?page=3"
+            "/test?page=4", "/test?page=5", "/test?page=6"]
+           (#'test-ns/extract-all-pager-links jsoup-snippet))
+        "extracts entire range of links")))
+
+(deftest extract-item-name-test
+  (is (= "test-item-name"
+         (#'test-ns/extract-item-name "http://www.summitpost.org/test-item-name/12345"))
+      "extracts item name from page"))
