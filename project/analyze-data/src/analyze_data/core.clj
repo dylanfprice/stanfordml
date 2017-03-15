0e92fa16e026455d4b01473c1a768f0854ec8a2e
Dylan Price <the.dylan.price@gmail.com>
2017-03-14 21:37:32 -0700

    Added top-level function to analyze-data.core to turn documents into tf-idf values.

--- project/analyze-data/src/analyze_data/core.clj
+++ project/analyze-data/src/analyze_data/core.clj
@@ -1,1 +1,11 @@
-(ns analyze-data.core)
+(ns analyze-data.core
+  (:require [analyze-data.tf-idf.core :refer [to-terms tf-idf]]
+            [analyze-data.tf-idf.)
+
+; (with-open [in-file (io/reader "../scrape-summitpost-data/data.csv")]
+;   (def row (apply zipmap (take 2 (csv/read-csv in-file)))))
+
+(defn get-tf-idf-corpus
+  [documents]
+  (let [term-corpus (map to-terms documents)]
+    (tf-idf term-corpus)))
