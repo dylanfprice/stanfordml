adf0c7cf08d9e54f6ed358ac12a135fea9752dba
Dylan Price <the.dylan.price@gmail.com>
2017-04-04 17:06:43 -0600

    Use (reduce into) instead of (apply concat) for non-lazy generation of all-terms.

--- project/analyze-data/src/analyze_data/tf_idf/core.clj
+++ project/analyze-data/src/analyze_data/tf_idf/core.clj
@@ -42,9 +42,8 @@
   corpus. The rest are sequences of tf-idf values of those terms, for each
   document in term-corpus."
   [term-corpus]
-  (let [all-terms (sort (distinct (apply concat term-corpus)))
+  (let [all-terms (sort (distinct (reduce into term-corpus)))
         tf-corpus (map double-normalized-term-frequency term-corpus)
         idf (inverse-document-frequency tf-corpus)
         tf-idf-values (map (partial tf-idf-document idf all-terms) tf-corpus)]
     (cons all-terms tf-idf-values)))
-
