32170b2593e1748413578a371774941acdb66e5a
Dylan Price <the.dylan.price@gmail.com>
2017-04-08 22:02:13 -0600

    Renamed return-value to result.

--- project/analyze-data/test/analyze_data/corpus_to_tf_idf_test.clj
+++ project/analyze-data/test/analyze_data/corpus_to_tf_idf_test.clj
@@ -5,12 +5,12 @@
 (deftest corpus-to-tf-idf-data-test
   (let [corpus [{"item-name" "foo" "item-text" "my dog rover"}
                 {"item-name" "bar" "item-text" "my dog spot"}]
-        return-value (test-ns/corpus-to-tf-idf-data corpus)
-        first-row (first return-value)
-        second-row (second return-value)]
-    (is (sequential? return-value)
+        result (test-ns/corpus-to-tf-idf-data corpus)
+        first-row (first result)
+        second-row (second result)]
+    (is (sequential? result)
         "returns a sequence")
-    (is (= 3 (count return-value))
+    (is (= 3 (count result))
         "contains same number of rows as input plus one header row")
     (is (= ["item-name" "dog" "dog rover" "dog spot" "rover" "spot"]
            first-row)
