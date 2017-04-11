a8e99e62bb70a1ca771164b04145eac01afe949b
Dylan Price <the.dylan.price@gmail.com>
2017-04-11 13:22:42 -0600

    Add docs and tests for find-knn.

--- project/analyze-data/src/analyze_data/find_knn.clj
+++ project/analyze-data/src/analyze_data/find_knn.clj
@@ -2,7 +2,7 @@
   (:require [clojure.core.matrix :as m]
             [clojure.edn :as edn]
             [clojure.java.io :as io]
-            [analyze-data.knn.core :refer [cosine-similarity knn]]
+            [analyze-data.knn.core :refer [cosine-distance knn]]
             [analyze-data.tf-idf.core :refer [tf-idf-document to-terms]]
             [analyze-data.gzip :refer [gzip-reader]])
   (:import java.io.PushbackReader))
@@ -19,7 +19,9 @@
         (cons (transform-fn line) (read-edn-stream reader transform-fn))))))
 
 (defn read-tf-idf-data
-  "TODO"
+  "Given the path to a file of gzipped tf-idf data as produced by
+  analyze-data.corpus-to-tf-idf/csv-corpus-to-tf-idf-data!, read into a map
+  with :idf, :all-terms, :item-names, and :data keys."
   [file-path]
   (with-open [file-reader (-> file-path
                               gzip-reader
@@ -30,23 +32,36 @@
      :data (m/sparse-matrix (read-edn-stream file-reader m/sparse-array))}))
 
 (defn knn-named-result
-  "TODO"
+  "Transform [index value] into [item-name value]."
   [item-names result]
   (let [index (first result)
         distance (second result)]
     [(nth item-names index) distance]))
 
 (defn document-to-vector
-  "TODO"
+  "Turn a document into a core.matrix/sparse-array of tf-idf values.
+
+  all-terms: order of features in vector
+  idf: map from term to inverse document frequency
+  document: string of document text"
   [all-terms idf document]
   (->> document (to-terms) (tf-idf-document all-terms idf) (m/sparse-array)))
 
 (defn find-knn
-  "TODO"
+  "Find 3 nearest neighbors to query-document in tf-idf-data.
+
+  tf-idf-data: map produced by read-tf-idf-data
+  query-document: string
+
+  Return sequence of the form
+  [[item-name distance]
+   [item-name distance]
+   [item-name distance]]"
   [tf-idf-data query-document]
   (let [{:keys [idf all-terms item-names data]} tf-idf-data
         query-vector (document-to-vector all-terms idf query-document)
         nearest-neighbors (knn data
                                query-vector
-                               :distance-fn cosine-similarity)]
+                               :k 3
+                               :distance-fn cosine-distance)]
     (map (partial knn-named-result item-names) nearest-neighbors)))
---
+++ project/analyze-data/test/analyze_data/find_knn_test.clj
@@ -0,0 +1,40 @@
+(ns analyze-data.find-knn-test
+  (:require [clojure.core.matrix :as m]
+            [clojure.test :refer [deftest is use-fixtures]]
+            [analyze-data.find-knn :as test-ns]))
+
+(defn use-vectorz-fixture
+  [f]
+  (let [default-implementation (m/current-implementation)]
+    (m/set-current-implementation :vectorz)
+    (f)
+    (m/set-current-implementation default-implementation)))
+
+(use-fixtures :once use-vectorz-fixture)
+
+(deftest knn-named-result-test
+  (is (= ["foo" 5]
+         (test-ns/knn-named-result ["foo"] [0 5]))
+      "transforms index into name"))
+
+(deftest document-to-vector-test
+  (let [all-terms ["bar" "foo"]
+        idf {"bar" 1, "foo" 2}
+        document-to-vector (partial test-ns/document-to-vector
+                                    all-terms
+                                    idf)]
+    (is (= [0.0 0.0]
+           (vec (document-to-vector "")))
+        "empty document returns 0 for all terms")
+    (is (= [1.0 0.0]
+           (vec (document-to-vector "bar")))
+        "document with a term returns a tf-idf value for that term")))
+
+(deftest find-knn-test
+  (let [tf-idf-data {:idf {"bar" 1, "foo" 1}
+                     :all-terms ["bar" "foo"]
+                     :item-names ["1" "2" "3"]
+                     :data [[1 1] [1 0.5] [1 0]]}]
+    (is (= ["1" "2" "3"]
+           (map first (test-ns/find-knn tf-idf-data "foo bar")))
+        "returns 3 nearest neighbors in order")))
