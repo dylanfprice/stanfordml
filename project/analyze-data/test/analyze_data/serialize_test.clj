b295c7bcaf68c53964e5416e19de8da3528ed0cd
Dylan Price <the.dylan.price@gmail.com>
2017-04-13 16:27:05 -0600

    Made test a little more clear by using string/blank?.

--- project/analyze-data/test/analyze_data/serialize_test.clj
+++ project/analyze-data/test/analyze_data/serialize_test.clj
@@ -1,12 +1,13 @@
 (ns analyze-data.serialize-test
   (:require [clojure.test :refer [deftest is]]
+            [clojure.string :as string]
             [analyze-data.serialize :as test-ns])
   (:import (java.io ByteArrayOutputStream ByteArrayInputStream)))
 
 (deftest write-object!-test
   (let [out (ByteArrayOutputStream.)]
     (test-ns/write-object! out "hello")
-    (is (not-empty (.toString out))
+    (is (not (string/blank? (.toString out)))
         "serializes object to stream")))
 
 (deftest read-object-test
