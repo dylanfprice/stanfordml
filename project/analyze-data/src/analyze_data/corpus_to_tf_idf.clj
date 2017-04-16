5c035f32ac53eb9d6dc17b1a5ae4f2039210ed4d
Dylan Price <the.dylan.price@gmail.com>
2017-04-15 19:39:32 -0600

    Move csv-to-map into its own namespace.

--- project/analyze-data/src/analyze_data/corpus_to_tf_idf.clj
+++ project/analyze-data/src/analyze_data/corpus_to_tf_idf.clj
@@ -1,18 +1,9 @@
 (ns analyze-data.corpus-to-tf-idf
   (:require [clojure.core.matrix :as m]
-            [clojure.data.csv :as csv]
             [clojure.java.io :as io]
+            [analyze-data.csv-to-map :refer [csv-to-map]]
             [analyze-data.serialize :refer [write-object!]]
             [analyze-data.tf-idf.core :refer [to-terms tf-idf]]))
-
-(defn csv-to-map
-  "Given an io/reader of a csv file, return a lazy sequence of maps from csv
-  header to data."
-  [reader]
-  (let [csv-file (csv/read-csv reader)
-        header-row (first csv-file)
-        data (rest csv-file)]
-    (map (partial zipmap header-row) data)))
 
 (defn create-sparse-matrix
   "Given
