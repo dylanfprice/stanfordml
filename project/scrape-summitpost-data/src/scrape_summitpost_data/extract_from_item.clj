88823d32cba758911a79152ed93c067161e6a787
Dylan Price <the.dylan.price@gmail.com>
2017-04-17 22:50:38 -0600

    Start on extract-children-links.

--- project/scrape-summitpost-data/src/scrape_summitpost_data/extract_from_item.clj
+++ project/scrape-summitpost-data/src/scrape_summitpost_data/extract_from_item.clj
@@ -1,5 +1,6 @@
 (ns scrape-summitpost-data.extract-from-item
-  (:require [reaver]))
+  (:require [clojure.string :as string]
+            [reaver]))
 
 (defn extract-item-name
   "Given the base url of summitpost a url to an item (such as a mountain),
@@ -13,3 +14,35 @@
   the main article."
   [page]
   (reaver/extract (reaver/parse page) [] "article" reaver/text))
+
+(defn- select-left-box-elements
+  [node]
+  (reaver/select node "#left_box div[class^=\"left_box\"]"))
+
+(defn- extract-left-box-heading
+  [node]
+  (reaver/extract node [] ".left_box_heading" reaver/text))
+
+(defn- is-children-heading?
+  [node]
+  (let [heading (or (extract-left-box-heading node) "")]
+    (string/starts-with? heading "Children")))
+
+(defn- extract-left-box-link
+  [node]
+  (reaver/extract
+    node
+    []
+    ".left_box_list_angle_bluegray > a" (reaver/attr :href)))
+
+(defn extract-children-links
+  "Given a string containing an item page from summitpost, return links to all
+  pages under the 'Children' header in the left sidebar."
+  [page]
+  (->> (reaver/parse page)
+       (select-left-box-elements)
+       (drop-while #(not (is-children-heading? %)))
+       (drop 1)
+       (take-while #(nil? (extract-left-box-heading %)))
+       (keep extract-left-box-link)))
+
