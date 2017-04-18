1c6ebc1e8a0360de72090a9449dbd804f72cc324
Dylan Price <the.dylan.price@gmail.com>
2017-04-17 22:24:40 -0600

    Refactored get-item-texts ns to more clearly separate each step of processing.

--- project/scrape-summitpost-data/src/scrape_summitpost_data/get_item_texts.clj
+++ project/scrape-summitpost-data/src/scrape_summitpost_data/get_item_texts.clj
@@ -9,40 +9,35 @@
 
 (def base-url "http://www.summitpost.org")
 
-(defn- get-pager-links
+(defn- get-pager-urls
   "Given a link to a paginated search results page from summitpost, GET the
-  page and return a sequence of links to all search results pages."
+  page and return a sequence of fully qualified urls to all search results
+  pages."
   [link]
-  (let [page (slurp (str base-url link))]
-    (or (extract-all-pager-links page) [link])))
+  (let [page (slurp (str base-url link))
+        pager-links (or (extract-all-pager-links page) [link])]
+    (map (partial str base-url) pager-links)))
 
-(defn- get-search-pages
-  "Given a link to a search results page from summitpost, GET every search
-  results page in the pagination (or just the page if no pagination) and
-  return them as a lazy sequence."
-  [link]
-  (->> (get-pager-links link)
-       (map (partial str base-url))
-       (map slurp)))
-
-(defn- get-urls
-  "Given a link to a search results page from summitpost, return a sequence of
-  fully qualified urls to all of the search result items. This function
-  performs multiple GET requests."
-  [link]
-  (->> link
-       (get-search-pages)
+(defn- get-result-urls
+  "Given a sequence of search results pages from summitpost (as strings),
+  return a sequence of fully qualified urls to all of the search result
+  items."
+  [search-pages]
+  (->> search-pages
        (map extract-result-links)
        (apply concat)
        (map (partial str base-url))))
 
 (defn get-item-texts
   "Given a link to a search results page from summitpost (i.e. relative to the
-  summitpost domain), GET all search result items (iterating through pagination
-  if present) and return a sequence of [item-url, item-name, item-text]
-  triples."
+  summitpost domain), GET all search result items (iterating through
+  pagination if present) and return a sequence of [item-url, item-name,
+  item-text] triples."
   [search-link]
-  (->> (get-urls search-link)
+  (let [pager-urls (get-pager-urls link)
+        search-pages (map slurp pager-urls)
+        result-urls (get-result-urls search-pages)]
+  (->> result-urls
        (map #(vector
                %
                (extract-item-name base-url %)
