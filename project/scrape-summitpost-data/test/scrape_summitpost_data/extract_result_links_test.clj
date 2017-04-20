0635d17d1aec0af212f43eb5aa91aa5663769b8e
Dylan Price <the.dylan.price@gmail.com>
2017-04-20 17:42:35 -0600

    Improve make-test-page to allow multiple urls and add multiple url test for extract-result-links.

--- project/scrape-summitpost-data/test/scrape_summitpost_data/extract_result_links_test.clj
+++ project/scrape-summitpost-data/test/scrape_summitpost_data/extract_result_links_test.clj
@@ -4,21 +4,27 @@
             [scrape-summitpost-data.extract-result-links :as test-ns]))
 
 (defn- make-test-page
-  [url]
-  (let [template "<table class='srch_results'>
-                    <tbody>
-                    <tr>
-                      <td class='srch_results_lft'></td>
-                      <td class='srch_results_rht'>
-                        <a href='{}'></a>
-                      </td>
-                    </tr>
-                    </tbody>
-                  </table>"]
-    (string/replace template "{}" url)))
+  [urls]
+  (let [header "<table class='srch_results'>
+                  <tbody>"
+        row-template "<tr>
+                        <td class='srch_results_lft'></td>
+                        <td class='srch_results_rht'>
+                          <a href='{}'></a>
+                        </td>
+                      </tr>"
+        footer "  </tbody>
+                </table>"
+        rows (mapv #(string/replace row-template "{}" %) urls)
+        page (conj (cons header rows) footer)]
+    (apply str page)))
 
 (deftest extract-result-links-test
-  (let [page (make-test-page "/test-item-name/12345")]
-    (is (= ["/test-item-name/12345"]
-           (test-ns/extract-result-links page))
-        "extracts link to item from search results table")))
+  (let [urls ["/test1"]]
+    (is (= urls
+           (test-ns/extract-result-links (make-test-page urls)))
+        "extracts single link from search results table"))
+  (let [urls ["/test1" "/test2" "/test3"]]
+    (is (= urls
+           (test-ns/extract-result-links (make-test-page urls)))
+        "extracts multiple links from search results table")))
