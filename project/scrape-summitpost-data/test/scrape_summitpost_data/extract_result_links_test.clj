f3086b81d4f53ef5f93020a1d5b7d3df89889029
Dylan Price <the.dylan.price@gmail.com>
2017-04-20 17:25:36 -0600

    Factored out test page into helper function.

--- project/scrape-summitpost-data/test/scrape_summitpost_data/extract_result_links_test.clj
+++ project/scrape-summitpost-data/test/scrape_summitpost_data/extract_result_links_test.clj
@@ -1,18 +1,24 @@
 (ns scrape-summitpost-data.extract-result-links-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.string :as string]
+            [clojure.test :refer [deftest is]]
             [scrape-summitpost-data.extract-result-links :as test-ns]))
 
+(defn- make-test-page
+  [url]
+  (let [template "<table class='srch_results'>
+                    <tbody>
+                    <tr>
+                      <td class='srch_results_lft'></td>
+                      <td class='srch_results_rht'>
+                        <a href='{}'></a>
+                      </td>
+                    </tr>
+                    </tbody>
+                  </table>"]
+    (string/replace template "{}" url)))
+
 (deftest extract-result-links-test
-  (let [page "<table class='srch_results'>
-                <tbody>
-                <tr>
-                  <td class='srch_results_lft'></td>
-                  <td class='srch_results_rht'>
-                    <a href='/test-item-name/12345'></a>
-                  </td>
-                </tr>
-                </tbody>
-              </table>"]
+  (let [page (make-test-page "/test-item-name/12345")]
     (is (= ["/test-item-name/12345"]
            (test-ns/extract-result-links page))
         "extracts link to item from search results table")))
