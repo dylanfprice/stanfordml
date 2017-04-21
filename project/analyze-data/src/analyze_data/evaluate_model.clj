75210e3e79b5aa1acbf070d5990b099185e7dbc0
Dylan Price <the.dylan.price@gmail.com>
2017-04-20 22:08:52 -0600

    Renamed evaluate-test-data to evaluate-model.

---
+++ project/analyze-data/src/analyze_data/evaluate_model.clj
@@ -0,0 +1,30 @@
+(ns analyze-data.evaluate-model
+  (:require [clojure.java.io :as io]
+            [analyze-data.csv-to-map :refer [csv-to-map]]
+            [analyze-data.find-knn :refer [find-knn]]
+            [analyze-data.serialize :refer [read-object]]))
+
+(defn evaluate-model
+  "Make a prediction for each row of test-data using given model and return (#
+  correct predictions / # rows of test data).
+
+  tf-idf-model: map produced by corpus-to-tf-idf-model
+  test-data: sequence of maps with 'document-name' and 'document-text' keys"
+  [tf-idf-model test-data]
+  (let [test-corpus (map #(get % "document-text") test-data)
+        test-labels (map #(get % "document-name") test-data)
+        predictions (map (partial find-knn tf-idf-model) test-corpus)
+        prediction-labels (map #(first (first %)) predictions)
+        correct-predictions (->> prediction-labels
+                                 (map #(vector %1 %2) test-labels)
+                                 (filter (partial apply =)))]
+    (float (/ (count correct-predictions) (count test-labels)))))
+
+(defn evaluate-model-file
+  "Like evaluate-model, but take file path arguments for both model and test
+  data."
+  [tf-idf-model-file test-file]
+  (with-open [in (io/reader test-file)]
+    (let [model (read-object tf-idf-model-file)
+          test-data (csv-to-map in)]
+      (evaluate-test-data tf-idf-model test-data))))
