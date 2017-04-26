a013abde867fb4b7a1225a50fa04050ded42f9d4
Dylan Price <the.dylan.price@gmail.com>
2017-04-25 22:11:06 -0600

    Add naive-bayes.predict with predict function and test.

---
+++ project/analyze-data/src/analyze_data/naive_bayes/predict.clj
@@ -0,0 +1,18 @@
+(ns analyze-data.naive-bayes.predict
+  (:require [clojure.core.matrix :as m]))
+
+(defn predict
+  "model: map produced by analyze-data.naive-bayes.train/train
+   x: vector of term weights representing a document
+
+  Return a sequence of the form
+  [[most-likely-label        probability]
+   [second-most-likely-label probability]
+   ...]"
+  [model x]
+  (let [{:keys [log-phi log-phi-y]} model
+        log-probs (->> (m/mul log-phi x)
+                       (map m/esum)
+                       (m/add log-phi-y))
+        indexed-log-probs (map-indexed #(vector %1 %2) log-probs)]
+    (reverse (sort-by second indexed-log-probs))))
---
+++ project/analyze-data/test/analyze_data/naive_bayes/predict_test.clj
@@ -0,0 +1,12 @@
+(ns analyze-data.naive-bayes.predict-test
+  (:require [clojure.core.matrix :as m]
+            [clojure.test :refer [deftest is]]
+            [analyze-data.naive-bayes.predict :as test-ns]))
+
+(deftest predict-test
+  (let [model {:log-phi (m/log [[1/2 1/4] [1/2 3/4]])
+               :log-phi-y (m/log [1/3 2/3])}
+        x [0 1]]
+    (is (= [[1 (+ (Math/log 2/3) (Math/log 3/4))]
+            [0 (+ (Math/log 1/3) (Math/log 1/4))]]
+            (test-ns/predict model x)))))
