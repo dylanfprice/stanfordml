03c388b10c434c5a4f6a0c4cb7838e5f67f6ddd0
Dylan Price <the.dylan.price@gmail.com>
2017-04-25 19:45:22 -0600

    Refactor evaluate-model ns.

--- project/analyze-data/src/analyze_data/evaluate_model.clj
+++ project/analyze-data/src/analyze_data/evaluate_model.clj
@@ -4,21 +4,38 @@
             [analyze-data.find-knn :refer [find-knn]]
             [analyze-data.serialize :refer [read-object]]))
 
+(defn get-predictions
+  "Make a prediction for each row of test-data using given model.
+
+  tf-idf-model: map produced by corpus-to-tf-idf-model
+  test-data: sequence of maps with 'document-name' and 'document-text' keys
+
+  Return a sequence of the form:
+  [[test-label [predicted-label distance]]
+    ...]"
+  [tf-idf-model test-data]
+  (let [test-corpus (map #(get % "document-text") test-data)
+        test-labels (map #(get % "document-name") test-data)
+        predictions (map (partial find-knn tf-idf-model) test-corpus)]
+    (map #(vector %1 (first %2)) test-labels predictions)))
+
+(defn- correct-prediction?
+  "Given a prediction of the form [test-label [predicted-label distance]],
+  return (= test-label predicted-label)."
+  [prediction]
+  (let [[test-label [predicted-label distance]] prediction]
+    (= test-label predicted-label)))
+
 (defn evaluate-model
   "Make a prediction for each row of test-data using given model and return (#
-  correct predictions / # rows of test data).
+  correct predictions / # total predictions).
 
   tf-idf-model: map produced by corpus-to-tf-idf-model
   test-data: sequence of maps with 'document-name' and 'document-text' keys"
   [tf-idf-model test-data]
-  (let [test-corpus (map #(get % "document-text") test-data)
-        test-labels (map #(get % "document-name") test-data)
-        predictions (map (partial find-knn tf-idf-model) test-corpus)
-        prediction-labels (map #(first (first %)) predictions)
-        correct-predictions (->> prediction-labels
-                                 (map #(vector %1 %2) test-labels)
-                                 (filter (partial apply =)))]
-    (float (/ (count correct-predictions) (count test-labels)))))
+  (let [predictions (get-predictions tf-idf-model test-data)
+        correct-predictions (filter correct-prediction? predictions)]
+    (float (/ (count correct-predictions) (count predictions)))))
 
 (defn evaluate-model-file
   "Like evaluate-model, but take file path arguments for both model and test
