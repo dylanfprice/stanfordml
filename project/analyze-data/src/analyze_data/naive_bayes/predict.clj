c2e0373404a1fd584596fa2a048232a423a5403f
Dylan Price <the.dylan.price@gmail.com>
2017-04-27 16:44:33 -0600

    Make predict return only one prediction.

--- project/analyze-data/src/analyze_data/naive_bayes/predict.clj
+++ project/analyze-data/src/analyze_data/naive_bayes/predict.clj
@@ -1,18 +1,23 @@
 (ns analyze-data.naive-bayes.predict
   (:require [clojure.core.matrix :as m]))
 
+(defn get-greater
+  "Given two vectors of the form [index value], return the one with the
+  greater value."
+  [a b]
+  (let [[a-index a-value] a
+        [b-index b-value] b]
+    (if (> a-value b-value) a b)))
+
 (defn predict
   "model: map produced by analyze-data.naive-bayes.train/train
    x: vector of term weights representing a document
 
-  Return a sequence of the form
-  [[most-likely-label        score]
-   [second-most-likely-label score]
-   ...]"
+  Return a vector of [most-likely-label score]"
   [model x]
   (let [{:keys [log-phi log-phi-y]} model
         log-probs (->> (m/mul log-phi x)
                        (map m/esum)
                        (m/add log-phi-y))
         indexed-log-probs (map-indexed #(vector %1 %2) log-probs)]
-    (reverse (sort-by second indexed-log-probs))))
+    (reduce get-greater indexed-log-probs)))
--- project/analyze-data/test/analyze_data/naive_bayes/predict_test.clj
+++ project/analyze-data/test/analyze_data/naive_bayes/predict_test.clj
@@ -3,10 +3,13 @@
             [clojure.test :refer [deftest is]]
             [analyze-data.naive-bayes.predict :as test-ns]))
 
+(deftest get-greater
+  (is (= [0 2]
+         (test-ns/get-greater [0 2] [1 1]))))
+
 (deftest predict-test
   (let [model {:log-phi (m/log [[1/2 1/4] [1/2 3/4]])
                :log-phi-y (m/log [1/3 2/3])}
         x [0 1]]
-    (is (= [[1 (+ (Math/log 2/3) (Math/log 3/4))]
-            [0 (+ (Math/log 1/3) (Math/log 1/4))]]
-            (test-ns/predict model x)))))
+    (is (= [1 (+ (Math/log 2/3) (Math/log 3/4))]
+           (test-ns/predict model x)))))
