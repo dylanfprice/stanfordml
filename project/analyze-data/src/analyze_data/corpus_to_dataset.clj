c4b91ea311b325a68b71d96b1402ec0b30e49c57
Dylan Price <the.dylan.price@gmail.com>
2017-04-27 23:03:09 -0600

    Created corpus-to-dataset multimethod to allow for multiple types of feature extraction methods to be tested and create a clean contract.

---
+++ project/analyze-data/src/analyze_data/corpus_to_dataset.clj
@@ -0,0 +1,62 @@
+(ns analyze-data.corpus-to-dataset
+  (:require [clojure.java.io :as io]
+            [analyze-data.csv-to-map :refer [csv-to-map]]
+            [analyze-data.create-sparse-matrix
+             :refer [create-sparse-matrix]]
+            [analyze-data.serialize :refer [write-object!]]
+            [analyze-data.tf-idf.core :refer [tf-idf to-terms]]))
+
+(defmulti corpus-to-dataset
+  "Transform a corpus of documents into a dataset.
+
+  corpus: a sequence of maps containing keys 'document-label' and
+          'document-text'
+  dataset-type: specifies the type of features to extract to make the
+                dataset.  Currently the only type is :tf-idf.
+
+  Return a dataset, which is a map with the following keys:
+  :type      the type of the dataset
+  :X         a core.matrix matrix of features where each row represents a
+             document and each column a feature
+  :y         a vector of indexes into :labels. The first entry will represent
+             the label of the first document in :X, and so on.
+  :features  a vector of names for each feature column
+  :labels    a vector of sorted distinct labels in the corpus
+  :extra     a map for type-specific extra data"
+  (fn [corpus feature-type] feature-type))
+
+(defn csv-corpus-to-dataset-file!
+  "Transform a csv containing a corpus of documents into a dataset file.
+
+  in: path to a csv file. It should contain the headers 'document-label' and
+      'document-text'
+  out: path where the dataset will be written as a serialized map.  You may
+       read this data back in using analyze-data.serialize/read-object, make
+       sure you use the same core.matrix implementation on each end.
+  dataset-type: the type of dataset to create"
+  [in out dataset-type]
+  (with-open [reader (io/reader in)]
+    (let [corpus (csv-to-map reader)
+          dataset (corpus-to-dataset corpus dataset-type)]
+      (write-object! out dataset))))
+
+(defn- get-y-and-labels
+  [corpus]
+  (let [document-labels (mapv #(% "document-label") corpus)
+        labels (vec (sort (distinct document-labels)))
+        lookup-label-index (reduce-kv (fn [m k v] (assoc m v k))
+                                      {}
+                                      labels)
+        y (mapv (partial lookup-label-index) document-labels)]
+    {:y y :labels labels}))
+
+(defmethod corpus-to-dataset :tf-idf [corpus feature-type]
+  (let [{:keys [y labels]} (get-y-and-labels corpus)
+        document-texts (map #(% "document-text") corpus)
+        {:keys [all-terms tf-idf idf]} (tf-idf (map to-terms document-texts))]
+    {:type feature-type
+     :X (create-sparse-matrix (count y) tf-idf)
+     :y y
+     :features all-terms
+     :labels labels
+     :extra {:inverse-document-frequencies idf}}))
---
+++ project/analyze-data/test/analyze_data/corpus_to_dataset_test.clj
@@ -0,0 +1,51 @@
+(ns analyze-data.corpus-to-dataset-test
+  (:require [clojure.core.matrix :as m]
+            [clojure.test :refer [deftest is testing use-fixtures]]
+            [analyze-data.corpus-to-dataset :as test-ns]
+            [analyze-data.test-fixtures :refer [use-vectorz]]))
+
+(use-fixtures :once use-vectorz)
+
+(deftest corpus-to-dataset-test
+  (let [corpus [{"document-label" "dog", "document-text" "my dog rover"}
+                {"document-label" "dog", "document-text" "my dog spot"}
+                {"document-label" "cat", "document-text" "my cat rover"}]
+        dataset-types [:tf-idf]]
+    (testing "for all dataset-types,"
+      (doseq [dataset-type dataset-types]
+        (let [result (test-ns/corpus-to-dataset corpus dataset-type)]
+          (is (map? result) "returns a map")
+          (is (every? #(contains? result %) [:type
+                                             :X
+                                             :y
+                                             :features
+                                             :labels
+                                             :extra])
+              "returned map contains all specced keys")
+          (is (= dataset-type (:type result)))
+          (is (= (count corpus) (first (m/shape (:X result))))
+              "X has # documents rows")
+          (is (= (count corpus) (count (:y result)))
+              "y has an entry for each document")
+          (is (= [1 1 0] (:y result))
+              "y contains label indexes for documents")
+          (is (= ["cat" "dog"] (:labels result))
+              "labels contains sorted distinct labels")
+          (is (map? (:extra result)) "extra is a map"))))
+    (testing ":tf-idf dataset-type"
+      (let [result (test-ns/corpus-to-dataset corpus :tf-idf)
+            double? #(= java.lang.Double (type %))]
+        (is (some? (first (:X result))) "X rows are not nil")
+        (is (every? double? (first (:X result)))
+            "elements of X rows are tf-idf values")
+        (is (= ["cat" "dog" "rover" "spot"] (:features result))
+            "features contains sorted terms")
+        (testing "has :extra map where"
+          (testing (str ":inverse-document-frequencies is map from term to "
+                        "inverse document frequency")
+            (is (= #{"cat" "dog" "rover" "spot"}
+                   (-> result :extra :inverse-document-frequencies keys set)))
+            (is (every? double? (-> result
+                                    :extra
+                                    :inverse-document-frequencies
+                                    vals)))))))))
