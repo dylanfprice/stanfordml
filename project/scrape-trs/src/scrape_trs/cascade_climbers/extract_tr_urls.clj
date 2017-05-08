7f8f9d8f14889c18c88c937f2506c9442afd8771
Dylan Price <the.dylan.price@gmail.com>
2017-05-07 23:12:28 -0600

    Only extract trip report urls from trip reports in Washington.

--- project/scrape-trs/src/scrape_trs/cascade_climbers/extract_tr_urls.clj
+++ project/scrape-trs/src/scrape_trs/cascade_climbers/extract_tr_urls.clj
@@ -1,17 +1,34 @@
 (ns scrape-trs.cascade-climbers.extract-tr-urls
   (:require [clojure.string :as string]
-            [reaver]
-            [scrape-trs.ensure-sequence :refer [ensure-sequence]]))
+            [reaver]))
+
+(def tr-table-index
+  "how many t_outer tables into the page the table with trip reports is"
+  9)
+
+(def washington-regions
+  #{"Alpine Lakes"
+    "Central/Eastern  Washington"
+    "Columbia River Gorge"
+    "Ice Climbing Forum"
+    "Mount Rainier NP"
+    "North Cascades"
+    "Olympic Peninsula"
+    "Rock Climbing Forum"
+    "Southern WA Cascades"
+    "the *freshiezone*"})
 
 (defn extract-tr-urls
   "Given a string containing the trip reports page from cascade climbers,
   return a sequence of urls representing the trip reports on that page."
   [page]
-  (->> (reaver/extract (reaver/parse page)
-                       []
-                       ".t_inner td a"
-                       (reaver/attr :href))
-      (ensure-sequence)
-      (filter (partial re-find
-                       #"/forum/ubbthreads\.php\?ubb=showflat&Number=\d+"))))
-
+  (let [tr-table (-> (reaver/select (reaver/parse page) ".t_outer")
+                     (nth tr-table-index))
+        table-rows (->> (reaver/select tr-table "td[class^=alt]")
+                        (partition 5))
+        wa-rows (filter (fn [[date tr-type location region posted-by]]
+                          (washington-regions (reaver/text region)))
+                        table-rows)
+        tr-urls (for [[date tr-type location region posted-by] wa-rows]
+                  (reaver/extract location [] "a" (reaver/attr :href)))]
+    tr-urls))
--- project/scrape-trs/test/scrape_trs/cascade_climbers/extract_tr_urls_test.clj
+++ project/scrape-trs/test/scrape_trs/cascade_climbers/extract_tr_urls_test.clj
@@ -1,57 +1,74 @@
 (ns scrape-trs.cascade-climbers.extract-tr-urls-test
-  (:require [clojure.test :refer [deftest is]]
+  (:require [clojure.string :as string]
+            [clojure.test :refer [deftest is]]
             [scrape-trs.cascade-climbers.extract-tr-urls :as test-ns]))
 
-(def page
-  "<table class='t_inner'>
-     <tbody>
-       <tr>
-         <td>
-           <a href='http://cascadeclimbers.com/forum/ubbthreads.php?ubb=tripreports&sb=2&page=1&amp;sk=0'>
-           Date
-           </a>
-         </td>
-         <td>
-           <a href='http://cascadeclimbers.com/forum/ubbthreads.php?ubb=tripreports&amp;sb=3&amp;page=1&amp;sk=0'>
-           Type
-           </a>
-         </td>
-         <td>
-           <a href='http://cascadeclimbers.com/forum/ubbthreads.php?ubb=tripreports&sb=5&page=1&sk=0'>
-           Location|Route
-           </a>
-         </td>
-       </tr>
-
-       <tr>
-         <td class='alt-1'>
-           4/23/2017
-         </td>
-         <td class='alt-1'>
-           Alpine
-         </td>
-         <td class='alt-1'>
-           <a href='http://cascadeclimbers.com/forum/ubbthreads.php?ubb=showflat&amp;Number=1154575'>
-           </a>
-         </td>
-       </tr>
-
-       <tr>
-         <td class='alt-2'>
-           12/13/2016
-         </td>
-         <td class='alt-2'>
-           Alpine
-         </td>
-         <td class='alt-2'>
-           <a href='http://cascadeclimbers.com/forum/ubbthreads.php?ubb=showflat&Number=1154544'>
-           </a>
-         </td>
-       </tr>
-     </tbody>
+(def page-template
+  "<table class='t_outer'></table>
+   <table class='t_outer'></table>
+   <table class='t_outer'></table>
+   <table class='t_outer'></table>
+   <table class='t_outer'></table>
+   <table class='t_outer'></table>
+   <table class='t_outer'></table>
+   <table class='t_outer'></table>
+   <table class='t_outer'></table>
+   <table class='t_outer'>
+     <tbody><tr><td>
+     <table>
+       <tbody>
+         <tr>
+           <td class='tdheader'>
+             <a href='http://some-header-url.com'>
+             Date
+             </a>
+           </td>
+           <!-- other headers here -->
+         </tr>
+         <tr>
+           <td class='{class}'>
+             4/23/2017
+           </td>
+           <td class='{class}'>
+             Alpine
+           </td>
+           <td class='{class}'>
+             <a href='{url}'>
+             </a>
+           </td>
+           <td class='{class}'>
+             {region}
+           </td>
+           <td class='{class}'>
+             <a href='http://some-user-url.com'>
+             </a>
+           </td>
+         </tr>
+       </tbody>
+     </table>
+     </td></tr></tbody>
    </table>")
 
+(defn make-test-page
+  [url & {:keys [css-class region]
+          :or {css-class "alt-1" region "Alpine Lakes"}}]
+  (-> page-template
+      (string/replace "{url}" url)
+      (string/replace "{class}" css-class)
+      (string/replace "{region}" region)))
+
 (deftest extract-tr-urls-test
-  (is (= ["http://cascadeclimbers.com/forum/ubbthreads.php?ubb=showflat&Number=1154575"
-          "http://cascadeclimbers.com/forum/ubbthreads.php?ubb=showflat&Number=1154544"]
-         (test-ns/extract-tr-urls page))))
+  (let [page (make-test-page "http://example.com/trip-reports/1234")]
+    (is (= ["http://example.com/trip-reports/1234"]
+           (test-ns/extract-tr-urls page))
+        "extracts only trip report urls"))
+  (let [page (make-test-page "http://example.com/trip-reports/1234"
+                             :css-class "alt-2")]
+    (is (= ["http://example.com/trip-reports/1234"]
+           (test-ns/extract-tr-urls page))
+        "extracts trip report urls from alt-2 table rows"))
+  (let [page (make-test-page "http://example.com/trip-reports/1234"
+                             :region "California")]
+    (is (= []
+           (test-ns/extract-tr-urls page))
+        "ignores trip reports outside of Washington")))
