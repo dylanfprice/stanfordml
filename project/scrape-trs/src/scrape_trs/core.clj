65d04b34a417deba08743462c9f33ec2a141ff7b
Dylan Price <the.dylan.price@gmail.com>
2017-05-11 21:39:05 -0600

    Some adjustments to get-trip-reports.

--- project/scrape-trs/src/scrape_trs/core.clj
+++ project/scrape-trs/src/scrape_trs/core.clj
@@ -1,23 +1,45 @@
 (ns scrape-trs.core
   (:require [scrape-trs.cascade-climbers.core :as cc]))
 
-(defn- get-implementation
-  [base-url]
-  (case base-url
-    cc/base-url (cc/->CCScrapeTripReport)))
+(comment
+  "TODO:
+   - add SPScrapeTripReport
+   - upgrade get-trip-reports to return [errors, trip-reports]
+     based on try-slurp that returns [error, contents]
+   - add docs and tests for get-trip-reports
+   - implement save-trip-reports!
+   - make scrape-wa-trs project that
+       retrieves cc.com tr list pages for each region in WA (POST)
+       retrieves sp.com tr list page for WA
+       get-trip-reports on each page and concat all together
+       save to csv"
+
+  (def cascade-climbers-washington-regions
+    #{"Alpine Lakes"
+      "Central/Eastern  Washington"
+      "Columbia River Gorge"
+      "Ice Climbing Forum"
+      "Mount Rainier NP"
+      "North Cascades"
+      "Olympic Peninsula"
+      "Rock Climbing Forum"
+      "Southern WA Cascades"
+      "the *freshiezone*"}))
+
+(defn get-implementation
+  [url]
+  (let [base-url (re-find #"https?://.*?/" url)]
+    (case base-url
+      "http://cascadeclimbers.com/" (cc/->CCScrapeTripReport))))
 
 (defn get-trip-reports
   ""
-  [list-page-url]
-  (let [base-url (re-find #"https?://.*?/" list-page-url)
-        implementation (get-implementation base-url)
-        list-page (slurp list-page)
-        pager-urls (.extract-pager-urls implementation list-page)
+  [implementation list-page]
+  (let [pager-urls (.extract-pager-urls implementation list-page)
         list-pages (map slurp pager-urls)
-        trip-report-urls (mapcat (partial .extract-trip-report-urls
-                                          implementation)
+        trip-report-urls (mapcat #(.extract-trip-report-urls implementation %)
                                  list-pages)
-        trip-reports (map (partial .extract-trip-report implementation)
+        trip-reports (map #(.extract-trip-report implementation %1 %2)
                           trip-report-urls
                           (map slurp trip-report-urls))]
     trip-reports))
