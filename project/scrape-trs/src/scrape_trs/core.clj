27ed6a2b73487fc961eab7b330ceade6425bebb4
Dylan Price <the.dylan.price@gmail.com>
2017-05-13 20:27:34 -0600

    Rework scrape-trs.core to be user-friendly.

--- project/scrape-trs/src/scrape_trs/core.clj
+++ project/scrape-trs/src/scrape_trs/core.clj
@@ -1,22 +1,35 @@
 (ns scrape-trs.core
-  (:require [scrape-trs.cascade-climbers.core :as cc]
+  (:require [clojure.string :as string]
+            [scrape-trs.cascade-climbers.core :as cc]
             [scrape-trs.summitpost.core :as sp]))
 
-(defn get-implementation
+(def ^:private implementations
+  {cc/base-url (cc/->CCScrapeTripReport)
+   sp/base-url (sp/->SPScrapeTripReport)})
+
+(defn list-supported-urls
+  "Return a sequence of the base urls of all sites with trip report scrapers."
+  []
+  (keys implementations))
+
+(defn- get-implementation
+  "Given a url whose base is in (list-supported-urls), return an
+  implementation of scrape-trs.protocol/ScrapeTripReport."
   [url]
-  (let [base-url (re-find #"https?://.*?/" url)]
-    (condp = base-url
-      cc/base-url (cc/->CCScrapeTripReport)
-      sp/base-url (sp/->SPScrapeTripReport))))
+  (second (first (filter #(string/starts-with? url (key %)) implementations))))
 
 (defn get-trip-reports
-  "implementation: type that implements scrape-trs.protocol/ScrapeTripReport
-  list-page: page (as a string) that lists trip reports and corresponds to the given implementation
+  "
+  url: url of list-page or the site it was retrieved from. Must correspond to
+       a supported url in (list-implementation-urls).
+  list-page: page (as a string) that lists trip reports
 
   Scrape all trip reports from the list-page, paging through pagination if
-  necessary, and return a sequence of scrape-trs.protocol/TripReport instances."
-  [implementation list-page]
-  (let [pager-urls (.extract-pager-urls implementation list-page)
+  necessary, and return a sequence of scrape-trs.protocol/TripReport
+  instances."
+  [url list-page]
+  (let [implementation (get-implementation url)
+        pager-urls (.extract-pager-urls implementation list-page)
         list-pages (if pager-urls (map slurp pager-urls) [list-page])
         trip-report-urls (mapcat #(.extract-trip-report-urls implementation %)
                                  list-pages)
