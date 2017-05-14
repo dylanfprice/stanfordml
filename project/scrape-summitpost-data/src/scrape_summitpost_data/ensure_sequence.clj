8e48d9bec90840c4e3fef62f00ba2ebfa43d0a15
Dylan Price <the.dylan.price@gmail.com>
2017-05-13 19:57:13 -0600

    Remove scrape-summitpost-data since it is now a part of scrape-trs.

--- project/scrape-summitpost-data/src/scrape_summitpost_data/ensure_sequence.clj
+++
@@ -1,7 +0,0 @@
-(ns scrape-summitpost-data.ensure-sequence)
-
-(defn ensure-sequence
-  "If arg is sequential?, return arg. Otherwise return a one-element vector
-  containing arg."
-  [arg]
-  (if (sequential? arg) arg [arg]))
