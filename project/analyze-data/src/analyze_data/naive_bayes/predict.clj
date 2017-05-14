2c23a54db1b281885bea477aab140eb447de7ff1
Dylan Price <the.dylan.price@gmail.com>
2017-05-14 12:01:15 -0600

    Add :threshold option to naive-bayes predictions.

--- project/analyze-data/src/analyze_data/naive_bayes/predict.clj
+++ project/analyze-data/src/analyze_data/naive_bayes/predict.clj
@@ -6,8 +6,13 @@
   "parameters: map produced by analyze-data.naive-bayes.train/train
    z: vector of term weights representing a document
 
-  Return a vector of [most-likely-label score]"
-  [parameters z]
+  Options:
+    :threshold (default 0.00) specify a probability that must be exceeded to
+               make a prediction (see below)
+
+  Return a vector of [most-likely-label probability] or [nil nil] if the
+  probability of the most likely label does not exceed :threshold."
+  [parameters z & options]
   ; n = # features
   ; k = # classes
   ; log-phi is k x n, log-phi-y is k x 1
@@ -19,6 +24,7 @@
   ;                 {\sum_{j=1}^k \exp[
   ;                    \sum_{i=1}^n \log p(z_i|y=j) + \log p(y=j) - C ]}
   (let [{:keys [log-phi log-phi-y]} parameters
+        {:keys [threshold] :or {threshold 0.00}} options
         log-numerators (->> (m/mul log-phi z)
                         (map m/esum)
                         (m/add log-phi-y))
@@ -27,5 +33,8 @@
                               (m/exp))
         scaled-denominator (m/esum scaled-numerators)
         probs (m/div scaled-numerators scaled-denominator)
-        indexed-probs (map-indexed #(vector %1 %2) probs)]
-    (reduce greatest indexed-probs)))
+        indexed-probs (map-indexed #(vector %1 %2) probs)
+        [most-likely-label probability] (reduce greatest indexed-probs)]
+    (if (> probability threshold)
+      [most-likely-label probability]
+      [nil nil])))
--- project/analyze-data/test/analyze_data/naive_bayes/predict_test.clj
+++ project/analyze-data/test/analyze_data/naive_bayes/predict_test.clj
@@ -9,7 +9,11 @@
         x [0 1]
         prediction (test-ns/predict parameters x)
         expected-class 1
-        expected-prob 6/7]
+        expected-prob (float 6/7)]
     (is (= expected-class (first prediction)))
-    (is (= (format "%.2f" (float expected-prob))
-           (format "%.2f" (second prediction))))))
+    (is (= (format "%.2f" expected-prob)
+           (format "%.2f" (second prediction))))
+    (is (= [nil nil]
+           (test-ns/predict parameters x, :threshold expected-prob))
+        (str "returns [nil nil] when prob of most likely label does not "
+             "exceed :threshold"))))
