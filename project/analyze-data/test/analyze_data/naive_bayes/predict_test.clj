0fe43eed0fc721abd8058f8d5b0952f0a7d6129e
Dylan Price <the.dylan.price@gmail.com>
2017-05-16 13:12:05 -0600

    Restructure test.

--- project/analyze-data/test/analyze_data/naive_bayes/predict_test.clj
+++ project/analyze-data/test/analyze_data/naive_bayes/predict_test.clj
@@ -1,19 +1,21 @@
 (ns analyze-data.naive-bayes.predict-test
   (:require [clojure.core.matrix :as m]
-            [clojure.test :refer [deftest is]]
+            [clojure.test :refer [deftest is testing]]
             [analyze-data.naive-bayes.predict :as test-ns]))
 
 (deftest predict-test
   (let [parameters {:log-phi (m/log [[1/2 1/4] [1/2 3/4]])
                     :log-phi-y (m/log [1/3 2/3])}
         x [0 1]
-        prediction (test-ns/predict parameters x)
         expected-class 1
         expected-prob (float 6/7)]
-    (is (= expected-class (first prediction)))
-    (is (= (format "%.2f" expected-prob)
-           (format "%.2f" (second prediction))))
-    (is (= [nil nil]
-           (test-ns/predict parameters x, :threshold expected-prob))
-        (str "returns [nil nil] when prob of most likely label does not "
-             "exceed :threshold"))))
+    (let [prediction (test-ns/predict parameters x)]
+      (is (= expected-class (first prediction))
+          "predicts the correct class")
+      (is (= (format "%.2f" expected-prob)
+             (format "%.2f" (second prediction)))
+          "calculates the correct probability"))
+    (testing ":threshold option"
+      (is (= [nil nil]
+             (test-ns/predict parameters x, :threshold 0.99))
+          "returns [nil nil] when prob does not exceed :threshold"))))
